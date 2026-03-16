65052a4c44ef6063448d69e0f0eb22dbf8ac8bf2
sulaman <Sulamanshahzad492@gmail.com>
2026-03-16 17:02:08 +0500

    uploading file through multer and cloudinary

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message="something went wrong ",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message)
         this.statusCode=statusCode
@@ -11,7 +11,7 @@ class ApiError extends Error{
         this.message=message
         this.success=false
         this.errors=errors
-        if(statck){
+        if(stack){
             this.stack=stack
         }else{
             Error.captureStackTrace(this,this.constructor)
