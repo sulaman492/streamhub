07e68f5e7c89587635762d24385962dc9bcd8ff9
sulaman <Sulamanshahzad492@gmail.com>
2026-03-17 17:09:13 +0500

    defining route and controller for user model

--- src/app.js
+++ src/app.js
@@ -11,5 +11,15 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,13 @@
-const asyncHandler=(fn)=async (req,res,next) => {
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(error.statusCode||500).json({
-            success:false,
-            message:error.message
-        })
+const asyncHandler = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            res.status(error.statusCode || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
     }
 }
 export default asyncHandler
